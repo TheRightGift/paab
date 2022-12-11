4eecd761a303ed2968046365c39f3b2475c7c9f6
Chigozie Chukwu <goziechukwu@gmail.com>
2022-12-11 11:09:15 +0100

    Template view section

---
+++ resources/js/components/templates/Physicians/HeaderComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="row" v-if="id == 1">
+        <p>Header for Template ID 1</p>
+    </div>
+
+    <div class="row" v-if="id == 2">
+        <p>Header for Template ID 2</p>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {};
+        },
+        props: {
+            id: String,
+        },
+        mounted() {},
+        methods: {
+            
+        },
+        computed: {},
+    };
+</script>
---
+++ resources/js/components/templates/Physicians/MainTemplateComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <HeaderComponent :id="1"/>
+    <IntroComponent :id="1"/>
+</template>
+<script>
+    import HeaderComponent from './HeaderComponent.vue';
+    import IntroComponent from './IntroComponent.vue';
+    export default {
+        components: {
+            HeaderComponent,
+            IntroComponent
+        },
+        data() {
+            return {};
+        },
+        props: {
+            id: Number,
+        },
+        mounted() {},
+        methods: {
+            
+        },
+        computed: {},
+    };
+</script>
