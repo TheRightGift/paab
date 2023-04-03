de9778e945294c209f45f4e2cf2f6df3996f73cd
Amaizu Somtoo <amaizupeter@yahoo.com>
2023-04-03 17:55:37 +0100

    Change for Email

---
+++ resources/views/mail/domain_claim.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <body>
+    <h3>Dr {{ $detail['name'] }}</h3>
+   
+    <h5>Your website is now live!</h5>
+
+    <p class="lead">
+        To login into the backend of your website to make FURTHER changes or updates please use the link and password below. 
+        When you login for the first time, you will be prompted to change your temporary password.
+    </p>
+
+    <p>Happy surfing!</p>
+
+    <p class="center">Temporary Password: {{ $detail['password'] }}</p>
+
+    <a href="https://{{$detail['domain']}}.whitecoatdomain.com/setting" target="_blank">https://{{$detail['domain']}}.whitecoatdomain.com/setting</a>
+
+    <p>Thank You</p>
+
+    <p>White Coat Domain</p>
+</body>
+</html>
+
+
