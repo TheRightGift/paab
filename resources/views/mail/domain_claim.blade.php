cb64d7d2679c0d653642a04a0d594318a472b64e
Amaizu Somtoo <amaizupeter@yahoo.com>
2023-05-30 11:09:41 +0100

    Changes to the mail

--- resources/views/mail/domain_claim.blade.php
+++ resources/views/mail/domain_claim.blade.php
@@ -1,43 +1,28 @@
 <!DOCTYPE html>
 <html>
     <body>
-    <h3>Dear Dr {{ $detail['name'] }},</h3>
-    <p>
-        Congratulations on the successful creation of your new website. It is truly a remarkable towards manging your brand as a physician and in line with our conviction that most of your patients are going to Google you so you should have some control over what they read about you.
-    </p>
-    <p>
-        Firstly, I would like to provide you with the login credentials for your website's backend:<br/>
+    <h3>Dr {{ $detail['names'] }}</h3>
+   
+    <h5>Your website is now live!</h5>
 
-        - Website URL: https://{{str_replace('.com', '', $detail['domain'])}}.whitecoatdomain.com (<a href="https://{{str_replace('.com', '', $detail['domain'])}}.whitecoatdomain.com" target="_blank">link</a>)<br/>
-        - Website backend URL: https://{{str_replace('.com', '', $detail['domain'])}}.whitecoatdomain.com/setting (<a href="https://{{str_replace('.com', '', $detail['domain'])}}.whitecoatdomain.com/setting" target="_blank">link</a>)<br/>
-        - Username: your email address<br/>
-        - Password: {{ $detail['password'] }}
-    </p>
-    <p>
-        Please ensure that you keep this information secure and accessible only to authorized individuals who require access to maintain your website.
-    </p>
-    <p>
-        I would also like to offer some suggestions for further upgrades and enhancements to your site, which can help enhance your users' experience and ensure its continued success:
-        <ul>
-            <li>
-            <b>Mini Blog:</b> You can offer your patients bite-sized content that delivers quick information or insights. You can use it to capture attention, provide value, and encourage further exploration of the topic through concise and visually appealing posts.
-            </li>
-            <li>
-            <b>Search Engine Optimization (SEO):</b> Optimize your website's content and structure to improve its visibility in search engine results. This will help potential visitors find your site more easily and increase organic traffic.
-            </li>
-            <li>
-            <b>Social Media Stream:</b> With this your website visitors can view and engage with your social media posts, updates, or feeds without leaving your website.
-            </li>
-            <li>
-            <b>Contact Information:</b> Provide contact information like email, address and phone number.
-            </li>
-        </ul>
+    <!-- <p class="lead">
+        To login into the backend of your website to make FURTHER changes or updates please use the link and password below. 
+        When you login for the first time, you will be prompted to change your temporary password.
+    </p> -->
+    <p>Your website @ http://{{$detail['domain']}}.com is now live for viewing and you can follow the previous email instruction to login and continue making changes</p>
+    <p class="lead">
+        To login into the backend of your website to make FURTHER changes or updates please use the link and password below. 
+        When you login for the first time, you will be prompted to change your temporary password.
     </p>
 
-    <p>Best regards,</p>
+    <p>Happy surfing!</p>
+
+    <p class="center">Temporary Password: {{ $detail['password'] }}</p>
+
+    <a href="https://{{$detail['domain']}}.com/setting" target="_blank">https://{{$detail['domain']}}.com/setting</a>
+
+    <p>Thank You</p>
 
     <p>White Coat Domain</p>
 </body>
 </html>
-
-
