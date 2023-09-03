a5adaa4136bcb14f90e2df34dc9a20ba4d0f7053
therightgift <goziechukwu@gmail.com>
2023-09-03 19:14:19 +0100

    updated beforeInstall bash

--- CICDScripts/before_install.sh
+++ CICDScripts/before_install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#download, insatll and integrate NGINX, LARAVEL, Composer, MySQL, PHPmyAdmin
-#TODO
-
-#creat wcdTest folder if not existing
+#create wcdTest folder if not existing
 DIR="/var/www/whitecoatdomain"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
